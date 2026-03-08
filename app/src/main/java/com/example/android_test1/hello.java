760f0371735e20dc3c4e3bd5f420a43c229b771b
khang <machgiakhang@gmail.com>
2026-03-08 12:52:21 -0400

    a3

--- app/src/main/java/com/example/android_test1/hello.java
+++ app/src/main/java/com/example/android_test1/hello.java
@@ -2,7 +2,8 @@ package com.example.android_test1;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello system 1");
-        System.out.println("hello system 2");
+        System.out.println("hello system 3");
+        System.out.println("hello system 2 bring the power");
+
     }
 }
