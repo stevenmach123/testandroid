675c6b5c45737e27354407fa1fc69d4dce5ef575
khang <machgiakhang@gmail.com>
2026-03-08 11:42:06 -0400

    s11

---
+++ app/src/main/java/com/example/android_test1/hello.java
@@ -0,0 +1,8 @@
+package com.example.android_test1;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello system");
+        System.out.println("hello system2");
+    }
+}
---
+++ app/src/main/java/com/example/android_test1/hello2.java
@@ -0,0 +1,7 @@
+package com.example.android_test1;
+
+public class hello2 {
+    public static void main(String[] args) {
+        System.out.println("hello system2");
+    }
+}
