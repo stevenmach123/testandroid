110dd7dd6176be1fff957c6ee8c0a355d4e9f7c5
khang <machgiakhang@gmail.com>
2026-03-08 11:43:33 -0400

    s12

--- app/src/main/java/com/example/android_test1/hello2.java
+++ app/src/main/java/com/example/android_test1/hello2.java
@@ -2,6 +2,7 @@ package com.example.android_test1;
 
 public class hello2 {
     public static void main(String[] args) {
-        System.out.println("hello system2");
+        System.out.println("hello2 system2");
+        System.out.println("hello2 system 2");
     }
 }
