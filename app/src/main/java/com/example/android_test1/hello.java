eae176d4397f74159c0d7737ee322ebcd163f345
khang <machgiakhang@gmail.com>
2026-03-08 18:24:59 -0400

    g1

--- app/src/main/java/com/example/android_test1/hello.java
+++ app/src/main/java/com/example/android_test1/hello.java
@@ -4,5 +4,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello system 1");
         System.out.println("hello system 2");
+
+        System.out.println("hello system gmae");
     }
 }
